7947d775b5e1a419a02b9b30aba6c68e47ce9820
Gileno Filho <contato@gilenofilho.com.br>
2018-06-14 15:03:25 -0300

    adicionando std desviation para o gráfico

---
+++ utils.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+def topology_std_desviation(g):
+    usage = []    
+    for edge in g.edges:
+        usage.apped(g[edge[0]][edge[1]]['LinkSpeedUsed'])
+    return pd.Series(usage).std()
